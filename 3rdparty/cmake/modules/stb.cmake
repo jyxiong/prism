3f39c4df8c65c3aaebafb86ac9f8b76dd44e5386
jyxiong <jingyu.xiong@foxmail.com>
2023-04-27 18:10:04 +0800

    init vulkan guide

--- 3rdparty/cmake/modules/stb.cmake
+++ 3rdparty/cmake/modules/stb.cmake
@@ -1,4 +1,4 @@
-file(GLOB stb_sources CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/stb/*.h)
+file(GLOB STB_SOURCES CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/stb/*.h)
 
-add_library(stb INTERFACE ${stb_sources})
+add_library(stb INTERFACE ${STB_SOURCES})
 target_include_directories(stb INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/stb)
