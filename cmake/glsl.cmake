ddfc5b5be2cd35a88c5aecc7d8a0594a2a4521b4
jyxiong <jingyu.xiong@foxmail.com>
2024-11-11 09:59:41 +0800

    chore: update third party

--- cmake/glsl.cmake
+++ cmake/glsl.cmake
@@ -1,11 +1,7 @@
-add_executable(glslang::validator IMPORTED)
-find_program(GLSLANG_VALIDATOR "glslangValidator" HINTS ${CMAKE_BINARY_DIR}/bin REQUIRED)
-set_property(TARGET glslang::validator PROPERTY IMPORTED_LOCATION ${GLSLANG_VALIDATOR})
-
 macro(compile_glsl GLSL_SHADER SPV_SHADER SPV_SHADERS)
     add_custom_command(
         OUTPUT ${SPV_SHADER}
-        COMMAND glslang::validator
+        COMMAND glslang-standalone
         ARGS ${GLSL_SHADER} --target-env vulkan1.3 -o ${SPV_SHADER} 
         MAIN_DEPENDENCY ${GLSL_SHADER}
         WORKING_DIRECTORY ${CMAKE_SHADERS_OUTPUT_DIRECTORY}
